3c8de465b03be9bab36d8d94a511522b754873c2
Ran <ran.hamdi@campus.technion.ac.il>
2025-01-06 02:58:14 +0200

    Change main to use semantic visitor

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "output.hpp"
 #include "nodes.hpp"
+#include "semantic.hpp"
 
 // Extern from the bison-generated parser
 extern int yyparse();
@@ -13,8 +15,13 @@ int main()
     // Parse the input. The result is stored in the global variable `program`
     yyparse();
 
-    // Print the AST using the PrintVisitor
-    output::PrintVisitor printVisitor;
-    program->accept(printVisitor);
+    // Print the AST using the PrintVisitor (for debug):
+    //output::PrintVisitor printVisitor;
+    //program->accept(printVisitor);
+
+    SemanticVisitor sv;
+    program->accept(sv);
+
+    std::cout << sv.symTable.getScopePrinter(); // print the output
     
 }
