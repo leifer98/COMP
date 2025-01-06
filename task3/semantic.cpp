56a0c2f36d89671c9d4d8ec1e49de89c086041ab
Ran <ran.hamdi@campus.technion.ac.il>
2025-01-06 02:56:48 +0200

    Add symbol table implementation and most visit functions implementations

---
+++ task3/semantic.cpp
@@ -0,0 +1,418 @@
+#include "semantic.hpp"
+#include <iostream>
+
+/* Helpful functions */
+
+bool isNumeric(ast::BuiltInType type) {
+    return (type == ast::BuiltInType::INT || type == ast::BuiltInType::BYTE);
+}
+
+bool isValidAssignment(ast::BuiltInType destType, ast::BuiltInType expType) {
+    return (destType == expType || (destType == ast::BuiltInType::INT && expType == ast::BuiltInType::BYTE));
+}
+
+
+/* Symbol Class */
+
+Symbol::Symbol(const std::string &name, ast::BuiltInType type, int offset, bool function)
+    : name(name), type(type), offset(offset), isFunction(function) {}
+
+
+/* VarSymbol Class */
+
+VarSymbol::VarSymbol(const std::string &name, ast::BuiltInType type, int offset)
+    : Symbol(name, type, offset, false) {}
+
+
+/* FuncSymbol Class */
+
+FuncSymbol::FuncSymbol(const std::string &name, ast::BuiltInType type, int offset, const std::vector<ast::BuiltInType> &paramTypes)
+    : Symbol(name, type, offset, true), paramTypes(paramTypes) {}
+
+
+/* ScopeSymbolTable Class */
+
+ScopeSymbolTable::ScopeSymbolTable(ScopeSymbolTable *parentTable) : parent(parentTable) {
+    if (parent) {
+        offset = parent->offset;
+    } else {
+        offset = 0;
+    }
+}
+
+int ScopeSymbolTable::declareVar(const std::string &name, ast::BuiltInType type, bool isParam) {
+    if (isParam) {
+        paramOffset--;
+
+        std::shared_ptr<VarSymbol> symbolPointer(new VarSymbol(name, type, paramOffset));
+        scopeSymbols.push_back(symbolPointer);
+
+        return paramOffset;
+    } else {
+        std::shared_ptr<VarSymbol> symbolPointer(new VarSymbol(name, type, offset));
+        scopeSymbols.push_back(symbolPointer);
+        offset++;
+
+        return offset - 1;
+    }
+}
+
+void ScopeSymbolTable::declareFunc(const std::string &name, ast::BuiltInType type, const std::vector<ast::BuiltInType> &paramTypes) {
+    std::shared_ptr<FuncSymbol> symbolPointer(new FuncSymbol(name, type, offset, paramTypes));
+    scopeSymbols.push_back(symbolPointer);
+}
+
+std::shared_ptr<Symbol> ScopeSymbolTable::lookup(const std::string &name) {
+    for (std::shared_ptr<Symbol> &symbolPointer : scopeSymbols) {
+        if (symbolPointer->name == name) {
+            return symbolPointer;
+        }
+    }
+
+    return std::shared_ptr<Symbol>(nullptr);
+}
+
+ScopeSymbolTable *ScopeSymbolTable::getParent() {
+    return parent;
+}
+
+
+/* SymbolTable Class */
+
+SymbolTable::SymbolTable() : scopePrinter() {
+    ScopeSymbolTable globalScope(nullptr);
+    scopesSymbolTables.push(globalScope);
+
+    declareFunc("print", ast::BuiltInType::VOID, {ast::BuiltInType::STRING});
+    declareFunc("printi", ast::BuiltInType::VOID, {ast::BuiltInType::INT});
+}
+
+void SymbolTable::declareVar(const std::string &name, ast::BuiltInType type, bool isParam) {
+    int varOffset = scopesSymbolTables.top().declareVar(name, type, isParam);
+
+    scopePrinter.emitVar(name, type, varOffset);
+}
+
+void SymbolTable::declareFunc(const std::string &name, ast::BuiltInType type, const std::vector<ast::BuiltInType> &paramTypes) {
+    scopesSymbolTables.top().declareFunc(name, type, paramTypes);
+
+    scopePrinter.emitFunc(name, type, paramTypes);
+}
+
+std::shared_ptr<Symbol> SymbolTable::lookup(const std::string &name) {
+    ScopeSymbolTable* currentScope = &scopesSymbolTables.top();
+
+    while (currentScope) {
+        std::shared_ptr<Symbol> symbol = currentScope->lookup(name);
+        if (symbol) {
+            return symbol;
+        }
+        currentScope = currentScope->getParent();
+    }
+
+    return std::shared_ptr<Symbol>(nullptr);
+}
+
+void SymbolTable::startScope() {
+    ScopeSymbolTable newScope(&scopesSymbolTables.top());
+    scopesSymbolTables.push(newScope);
+
+    scopePrinter.beginScope();
+}
+
+void SymbolTable::endScope() {
+    scopesSymbolTables.pop();
+
+    scopePrinter.endScope();
+}
+
+output::ScopePrinter& SymbolTable::getScopePrinter() {
+    return scopePrinter;
+}
+
+
+/* SemanticVisitor implementation */
+
+void SemanticVisitor::visit(ast::Num &node) {
+    
+    node.type = ast::BuiltInType::INT;
+}
+
+void SemanticVisitor::visit(ast::NumB &node) {
+    
+    node.type = ast::BuiltInType::BYTE;
+}
+
+void SemanticVisitor::visit(ast::String &node) {
+    
+    node.type = ast::BuiltInType::STRING;
+}
+
+void SemanticVisitor::visit(ast::Bool &node) {
+    
+    node.type = ast::BuiltInType::BOOL;
+}
+
+void SemanticVisitor::visit(ast::ID &node) {
+
+    std::shared_ptr<Symbol> symbol = symTable.lookup(node.value);
+    if (symbol) {  // The identifier exists
+        if (symbol->isFunction) {
+            output::errorDefAsFunc(node.line, node.value);
+        } else if (node.isDeclaration) {
+            output::errorDef(node.line, node.value);
+        } else {
+            node.type = symbol->type;
+        }
+    } else {
+        if (!node.isDeclaration) {
+            if (node.isFunctionCall) {
+                output::errorUndefFunc(node.line, node.value);
+            } else {
+                output::errorUndef(node.line, node.value);
+            }
+        }
+    }
+    
+}
+
+void SemanticVisitor::visit(ast::BinOp &node) {
+
+    node.left->accept(*this);
+    node.right->accept(*this);
+
+    if (isNumeric(node.left->type) && isNumeric(node.right->type)) {
+        // Choose the expression type to be the type of the two which has larger range of expression
+        // (int is larger than byte so choose it if it exists in the expression)
+        if (node.left->type == ast::BuiltInType::INT || node.right->type == ast::BuiltInType::INT) {
+            node.type = ast::BuiltInType::INT;
+        } else {
+            node.type = ast::BuiltInType::BYTE;
+        }
+    } else {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::RelOp &node) {
+
+    node.left->accept(*this);
+    node.right->accept(*this);
+
+    // Type check:
+    if (isNumeric(node.left->type) && isNumeric(node.right->type)) {
+        node.type = ast::BuiltInType::BOOL;
+    } else {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::Type &node) {
+    
+}
+
+void SemanticVisitor::visit(ast::Cast &node) {
+
+}
+
+void SemanticVisitor::visit(ast::Not &node) {
+
+    node.exp->accept(*this);
+
+    if (node.exp->type == ast::BuiltInType::BOOL) {
+        node.type = ast::BuiltInType::BOOL;
+    } else {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::And &node) {
+    
+    node.left->accept(*this);
+    node.right->accept(*this);
+
+    if (node.left->type == ast::BuiltInType::BOOL && node.right->type == ast::BuiltInType::BOOL) {
+        node.type = ast::BuiltInType::BOOL;
+    } else {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::Or &node) {
+
+    node.left->accept(*this);
+    node.right->accept(*this);
+
+    if (node.left->type == ast::BuiltInType::BOOL && node.right->type == ast::BuiltInType::BOOL) {
+        node.type = ast::BuiltInType::BOOL;
+    } else {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::ExpList &node) {
+
+    for (std::shared_ptr<ast::Exp> exp: node.exps) {
+        exp->accept(*this);
+    }
+}
+
+void SemanticVisitor::visit(ast::Call &node) {
+    
+    // node.func_id->accept(*this);
+
+    // node.args->accept(*this);
+}
+
+void SemanticVisitor::visit(ast::Statements &node) {
+
+    // Open scope for all blocks except function bodies (functions need to include params in their scope):
+    if (!node.isFuncBody) {
+        symTable.startScope();
+    }
+    
+    // Visit all statements:
+    for (std::shared_ptr<ast::Statement> statement: node.statements) {
+        statement->accept(*this);
+    }
+
+    // Close scope for all blocks except function bodies:
+    if (!node.isFuncBody) {
+        symTable.endScope();
+    }
+}
+
+void SemanticVisitor::visit(ast::Break &node) {
+
+    if (!node.isInWhile) {
+        output::errorUnexpectedBreak(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::Continue &node) {
+
+    if (!node.isInWhile) {
+        output::errorUnexpectedContinue(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::Return &node) {
+
+    // if (node.exp) {
+    //     node.exp->accept(*this);
+    // }
+}
+
+void SemanticVisitor::visit(ast::If &node) {
+    
+    node.condition->accept(*this);
+
+    symTable.startScope();
+    node.then->accept(*this);
+    symTable.endScope();
+
+    if (node.otherwise) {
+        symTable.startScope();
+        node.otherwise->accept(*this);
+        symTable.endScope();
+    }
+    
+    // Type checking:
+    if (node.condition->type != ast::BuiltInType::BOOL) {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::While &node) {
+
+    node.condition->accept(*this);
+
+    symTable.startScope();
+    node.body->isInWhile = true;
+    node.body->accept(*this);
+    symTable.endScope();
+
+    // Type checking:
+    if (node.condition->type != ast::BuiltInType::BOOL) {
+        output::errorMismatch(node.line);
+    }
+}
+
+void SemanticVisitor::visit(ast::VarDecl &node) {
+
+    node.type->accept(*this);
+    if (node.init_exp) {
+        node.init_exp->accept(*this);
+        
+        if (!isValidAssignment(node.type->type, node.init_exp->type)) {
+            output::errorMismatch(node.line);
+        }
+    }
+
+    // Give the id node the data that it's a declaration of a var and than visit it:
+    node.id->isDeclaration = true;
+    node.id->accept(*this);
+
+    symTable.declareVar(node.id->value, node.type->type, false);
+}
+
+void SemanticVisitor::visit(ast::Assign &node) {
+
+    // node.id->accept(*this);
+
+    // node.exp->accept(*this);
+}
+
+void SemanticVisitor::visit(ast::Formal &node) {
+
+    node.type->accept(*this);
+
+    // Give the id node the data that it's a declaration of a var and than visit it:
+    node.id->isDeclaration = true;
+    node.id->accept(*this);
+
+    symTable.declareVar(node.id->value, node.type->type, true);
+}
+
+void SemanticVisitor::visit(ast::Formals &node) {
+
+    for (std::shared_ptr<ast::Formal> formal: node.formals) {
+        formal->accept(*this);
+    }
+}
+
+void SemanticVisitor::visit(ast::FuncDecl &node) {
+
+    node.return_type->accept(*this);
+    node.id->type = node.return_type->type;
+    
+    symTable.startScope();
+
+    node.formals->accept(*this);
+
+    node.body->isFuncBody = true;
+    node.body->accept(*this);
+    
+    symTable.endScope();
+
+    // TODO check that return type match
+}
+
+void SemanticVisitor::visit(ast::Funcs &node) {
+
+    // Declare all functions:
+    for (std::shared_ptr<ast::FuncDecl> func: node.funcs) {
+        // Get a vector of param types:
+        std::vector<ast::BuiltInType> paramTypes;
+        for (std::shared_ptr<ast::Formal> &formal : func->formals->formals) {
+            paramTypes.push_back(formal->type->type);
+        }
+
+        symTable.declareFunc(func->id->value, func->return_type->type, paramTypes);
+    }
+
+    // Visit all functions:
+    for (std::shared_ptr<ast::FuncDecl> func: node.funcs) {
+        func->accept(*this);
+    }
+}
